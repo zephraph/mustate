afe78a748f5da91a2b28fcca508de6fe46fa50e5
Justin Bennett <justin.bennett@norm.ai>
2025-11-01 17:23:36 -0400

    Convert to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: ["esm"],
 	dts: true,
 	clean: true,
-	bundle: false,
+	unbundle: true,
 });
